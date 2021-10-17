9f0d7636e7e56fae890c33ad7774106616f87a71
Saifullah Chowdhury <saifullahchy0177@gmail.com>
2021-10-17 11:37:31 +0600

    commit before admin panel created

--- src/components/contactPage/index.jsx
+++ src/components/contactPage/index.jsx
@@ -1,19 +1,7 @@
 import React,{ Component } from 'react'
-// import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
 import './contactElements.css'
 
-// function Map(){
-//     return (
-//       <GoogleMap  defaultZoom={20}
-//                 defaultCenter={{lat:23.773201, lng:90.413347}}
-                
-//       />
-     
-//     )
-// }
-// const  WrappedMap = withScriptjs(withGoogleMap(Map))
 
- 
 
 const Contact = () => {
     return (
@@ -47,13 +35,7 @@ const Contact = () => {
                     </div>
               </div>
               <div className="cont-map" >
-               {/* <WrappedMap 
-               googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBrQ98WJ9ysUumIQIMczSR0BjbpyCB_6o8&v=3.exp&libraries=geometry,drawing,places`}
-               loadingElement={<div style={{ height: `100%` }} />}
-               containerElement={<div style={{ height: `500px` }} />}
-               mapElement={<div style={{ height: `100%` }} />}
-                />
-              </div> */}
+               
 <div ><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=westford%20immigration+(Westford)&amp;t=&amp;z=19&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
           </div>
       </div>
